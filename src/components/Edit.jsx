89da0f8d71ed79a4623c54d04565680fb576dffa
unknown <ramyaachar1998@gmail.com>
2022-07-13 09:52:18 +0530

    first commit

---
+++ src/components/Edit.jsx
@@ -0,0 +1,95 @@
+import { useRef, useState } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const Edit = () => {
+
+    let history = useHistory();
+
+    const { id } = useParams();
+
+    let { data: task } = useFetch("http://localhost:4000/tasks/" + id);
+
+    // let uName = useRef("");
+    // let uTaskName = useRef("");
+    // let uTaskDetail = useRef("");
+    // let uStartDate = useRef("");
+    // let uEndDate = useRef("");
+
+    const [userName,setuserName]=useState("")
+    // const [taskName, settaskName]=useState("")
+    // const [taskDetail,settaskDetail]=useState("")
+    // const [startDate, setstartDate]=useState("")
+    // const [endDate, setendDate]=useState("")
+
+
+    const handleEdit = (e) => {
+        e.preventDefault();
+
+        let currentDate = new Date();
+        let startDate = new Date();
+        let endDate = new Date();
+        let status = "";
+
+        if (currentDate < startDate) {
+            status = "Pending";
+        }
+        else if (currentDate >= startDate && currentDate <= endDate) {
+            status = "Ongoing"
+        }
+        else {
+            status = "Completed";
+        }
+        let newTask  = {userName }
+
+        // {, taskName , taskDetail , startDate , endDate}
+        // console.log(newTask);
+        
+        fetch("http://localhost:4000/tasks/" + id,
+            {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newTask)
+            }
+        ).then(() => {
+            alert("task edited successfully");
+            history.push("/tasklist");
+        })
+    }
+
+
+    return (
+        <div className="edit-task">
+
+            <h1>Edit a Task</h1>
+
+            {task && <div>
+                <form onSubmit={handleEdit}>
+
+                    <label>User Name</label> <input type="text" defaultValue={task.userName} onChange={(e)=>{setuserName(e.target.value)}}  />
+
+                    <label>Task Name</label> <input type="text" defaultValue={task.taskName} />
+                    {/* onChange={(e)=>(settaskName(e.target.value))}  */}
+
+                    <label>Task Detail</label> <textarea defaultValue={task.taskDetail} />
+                    {/* onChange={(e)=>{settaskDetail(e.target.value)}}  */}
+
+                    <label>Start Date</label> <input type="date" defaultValue={task.startDate} />
+                    {/* onChange={(e)=>{setstartDate(e.target.value)}}  */}
+
+                    <label>End Date</label> <input type="date" defaultValue={task.endDate}  />
+                    {/* onChange={(e)=>{setendDate(e.target.value)}} */}
+
+                    <input type="submit" value="Edit Task" />
+
+                </form>
+
+                <Link to="/tasklist">
+                    <button className="btn">Click to view all task list</button>
+                </Link>
+            </div>}
+
+        </div>
+    );
+}
+export default Edit;
